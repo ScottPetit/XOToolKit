836eaffd064ef9c4ff084d28df89a289c5e75874
Scott Petit <scott.petit@bottlerocketapps.com>
2013-05-19 20:09:13 -0500

    - adds flat ui colors

--- XOToolKit/XOToolKit/Categories/UIColor+XO.m
+++ XOToolKit/XOToolKit/Categories/UIColor+XO.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIColor+XO.h"
+#import "XOUtility.h"
 
 @implementation UIColor (XO)
 
@@ -49,6 +50,108 @@
 - (CGFloat)alpha
 {
     return CGColorGetAlpha(self.CGColor);
+}
+
+#pragma mark - Flat Colors
+
++ (UIColor *)turquoiseColor
+{
+    return RGB(26, 188, 156);
+}
+
++ (UIColor *)greenSeaColor
+{
+    return RGB(22, 160, 133);
+}
+
++ (UIColor *)emerlandColor
+{
+    return RGB(46, 204, 113);
+}
+
++ (UIColor *)nephritisColor
+{
+    return RGB(39, 174, 96);
+}
+
++ (UIColor *)peterRiverColor
+{
+    return RGB(52, 152, 219);
+}
+
++ (UIColor *)belizeHoleColor
+{
+    return RGB(41, 128, 185);
+}
+
++ (UIColor *)amethystColor
+{
+    return RGB(155, 89, 182);
+}
+
++ (UIColor *)wisteriaColor
+{
+    return RGB(142, 68, 173);
+}
+
++ (UIColor *)wetAsphaltColor
+{
+    return RGB(52, 73, 94);
+}
+
++ (UIColor *)midnightBlueColor
+{
+    return RGB(44, 62, 80);
+}
+
++ (UIColor *)sunflowerColor
+{
+    return RGB(241, 196, 15);
+}
+
++ (UIColor *)tangerineColor
+{
+    return RGB(243, 156, 18);
+}
+
++ (UIColor *)carrotColor
+{
+    return RGB(230, 126, 34);
+}
+
++ (UIColor *)pumpkinColor
+{
+    return RGB(211, 84, 0);
+}
+
++ (UIColor *)alizarinColor
+{
+    return RGB(231, 76, 60);
+}
+
++ (UIColor *)pomegranateColor
+{
+    return RGB(192, 57, 43);
+}
+
++ (UIColor *)cloudsColor
+{
+    return RGB(236, 240, 241);
+}
+
++ (UIColor *)silverColor
+{
+    return RGB(189, 195, 199);
+}
+
++ (UIColor *)concreteColor
+{
+    return RGB(149, 165, 166);
+}
+
++ (UIColor *)asbestosColor
+{
+    return RGB(127, 140, 141);
 }
 
 @end
